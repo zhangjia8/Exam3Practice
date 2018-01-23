52e1bcf577ea87e30cd344993295cd9b198672d1
zhangj8 <zhangj8@rose-hulman.edu>
2018-01-23 16:22:36 -0500

    m4

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  SEQUENCES.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jiatong Zhang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -134,13 +134,18 @@ def practice_problem4a(sequence):
       :type sequence: list | tuple | string
     """
     ####################################################################
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
+    n = []
+    for k in range(len(sequence) - 1):
+        if sequence[k] == sequence[k + 1]:
+            n += [k]
+    return n
 
 
 def run_test_practice_problem4b():
@@ -197,13 +202,18 @@ def practice_problem4b(sequence):
       :type sequence: (list | tuple) of (float | int)
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   10 minutes.
     ####################################################################
+    n = sequence[0]
+    for k in range(2, len(sequence), 2):
+        if sequence[k] > n:
+            n = sequence[k]
+    return n
 
 
 def run_test_practice_problem4c():
@@ -295,7 +305,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -306,6 +316,14 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
+    for k in range(len(points)):
+        x = points[k].x
+        y = points[k].y
+        if is_prime(x) is True and is_prime(y) is True:
+            points[k].x = y
+            points[k].y = x
+            return points[k]
+    return 'Not found'
 
 
 def run_test_practice_problem4d():
@@ -391,14 +409,19 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ####################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
-
+    su = 0
+    for k in range(len(sequence) - 1):
+        if is_prime(sequence[k]) is True and is_prime(sequence[k + 1]) is True:
+            if sequence[k] != sequence[k + 1]:
+                su += sequence[k]
+    return su
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
